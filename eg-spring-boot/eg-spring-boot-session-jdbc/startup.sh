e84c5973e378752b994f8afaa92611cb14f5175b
lenicliu <lenicliu@163.com>
2016-06-12 12:33:59 +0800

    react spring boot session

--- eg-spring-boot/eg-spring-boot-session-jdbc/startup.sh
+++
@@ -1,3 +0,0 @@
-java -cp h2.jar org.h2.tools.Server -tcp -baseDir . &
-java -Xms64M -Xmx64M -jar target/eg-spring-boot-session-jdbc-0.0.1-SNAPSHOT.jar --server.port=8080 &
-java -Xms64M -Xmx64M -jar target/eg-spring-boot-session-jdbc-0.0.1-SNAPSHOT.jar --server.port=8081 &
